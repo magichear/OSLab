334acfe316424081a05ddcc50653464f75830d24
magichear <wgdecade43@mail.ustc.edu.cn>
2024-04-15 18:04:21 +0800

    Finished create

--- Lab2/SysCall_part/auto_compile.sh
+++ Lab2/SysCall_part/auto_compile.sh
@@ -4,7 +4,7 @@ echo plz check if any problem
 echo Enter to continue
 echo Ctrl and C to quit
 read dummy_variable
-sudo cp -f ~/oslab/Lab2/SysCall_part/my_get_p_c ~/oslab/busybox-1.32.1/_install/
+sudo cp -f ~/oslab/OSlab/Lab2/SysCall_part/my_get_p_c ~/oslab/busybox-1.32.1/_install/
 cd ~/oslab/busybox-1.32.1/_install/
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > ~/oslab/initramfs-busybox-x64.cpio.gz
 cd ~/oslab/linux-4.9.263/
