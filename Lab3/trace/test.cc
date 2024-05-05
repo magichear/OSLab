918b92e74166af741f3a6335e5729e3e05a89a33
magichear <zxy@zxy0.myguest.virtualbox.org>
2024-05-05 23:27:34 +0800

    Normal update1

---
+++ Lab3/trace/test.cc
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"before free: "<<std::endl;
+    return 0;
+}
---
+++ test/test_pthread.c
@@ -0,0 +1,34 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/syscall.h>
+#include<sys/wait.h>
+#include<stdio.h>
+
+int value = 0;
+void *runner(void *param); /* the thread */
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+    pthread_t tid;
+    pthread_attr_t attr;
+    pid = fork();
+
+    if (pid == 0) { /* child process */
+        pthread_attr_init(&attr);
+        pthread_create(&tid, &attr, runner, NULL);
+        pthread_join(tid, NULL);
+        printf("CHILD: value = %d\n", value);   /* LINE C */
+    }
+    else if (pid > 0) { /* parent process */
+        wait(NULL);
+        printf("PARENT: value = %d\n\n", value);  /* LINE P */
+    }
+    return 0;
+}
+
+void *runner(void *param) {
+    value = 5;
+    pthread_exit(0);
+}
