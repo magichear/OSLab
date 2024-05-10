802cec0ebbaf06b5f09af0c15c244336c56bbed1
MJ <wgdecade43@mail.ustc.edu.cn>
2024-05-10 19:08:09 +0800

    2024.5.10 Finish Lab3 and Lab4

--- GitPush.bat
+++ GitPush.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting auto commit and push to git
 git add .
-git commit -m "Normal update1"
+git commit -m "2024.5.10 Finish Lab3 and Lab4"
 git push
 echo -----------------
 echo -Task completed--
